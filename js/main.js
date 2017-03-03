d984b932109d6c3054bcff8ce1eb2facc5a0604b
Oleg Zinchenko <olegzinchenko1983@gmail.com>
2017-03-03 15:35:48 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -6,4 +6,24 @@ $(document).ready(function(){
 		$(this).removeClass('active');
 	});
 
+	$('ul.nav li:first-child').click(function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop: $('#chicken').offset().top-20},800);
+	});
+
+	$('ul.nav li:nth-child(2)').click(function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop: $('#beef').offset().top-20},800);
+	});
+
+	$('ul.nav li:last-child').click(function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop: $('#sushi').offset().top-20},800);
+	});
+
+	$('section span a').click(function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop: $('#top').offset().top},800);
+	});
+
 });
